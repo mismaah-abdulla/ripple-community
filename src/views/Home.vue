e8a56ae929a5ed50e964d0a9f55f6af3ac0a5f19
mismaah-abdulla <mismaahabdulla@gmail.com>
2019-09-18 14:55:04 +0800

    add parallax

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,31 @@
 <template>
   <!-- <HelloWorld /> -->
   <v-container>
+    <v-parallax id="parallax-ripple"
+      dark
+      height="300"
+      src="https://miro.medium.com/max/2250/1*lBOjneH9fSUjPgM4DrkP0A.jpeg"
+    >
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <h1 class="display-1 mb-4">Mental Health & Self Help • Wisdom & Inspiration • Life Style & Well being</h1>
+      </v-row>
+    </v-parallax>
     <v-row justify="center">
-      <img src="/logo.webp">
+      <v-card><img src="/logo.webp"></v-card>
+      
     </v-row>
   </v-container>
 </template>
+
+<style>
+  /* #parallax-ripple img {
+    height: 100%
+  } */
+</style>
+
 
 <script>
 // import HelloWorld from '../components/HelloWorld'
