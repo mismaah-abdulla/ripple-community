142e018f4b615b6b3f259126632e584376a7fe5a
mismaah-abdulla <mismaahabdulla@gmail.com>
2019-09-29 17:36:07 +0800

    add video to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row justify="center" class="py-5">
+      <h1 class="display-2">The story behind ripple.</h1>
+    </v-row>
+    <v-row justify="center">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Nbp8XI_HZaU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </v-row>
+  </v-container>
 </template>
