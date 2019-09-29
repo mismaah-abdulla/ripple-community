d9d8841f7ae03ce967a1bb5caed3972e30591878
mismaah-abdulla <mismaahabdulla@gmail.com>
2019-09-29 17:26:52 +0800

    modify contact us page

---
+++ src/views/ContactUs.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+      <v-row justify="center" class="py-5">
+          <h1 class="headline">Make ripples with us!</h1>
+      </v-row>
+      <v-row justify="center">
+          <h1 class="subtitle-1">Contact us at email@email.com <br/> Or message us on social media!</h1>
+      </v-row>
+  </v-container>
+</template>
