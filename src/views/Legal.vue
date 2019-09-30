79b669414a8301abdd5e9e33052605f0545a38a3
mismaah-abdulla <mismaahabdulla@gmail.com>
2019-09-30 13:27:53 +0800

    add all legal disclaimers

--- src/views/Legal.vue
+++ src/views/Legal.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container px-12 style="width: 60%">
-        <v-row justify="center">
+        <v-row justify="center" class="py-5">
             <h1 class="display-1 font-weight-light">Legal Disclaimers<br></h1>
         </v-row>
         <v-row>
-            <h2 class="font-weight-light headline">Privacy Policy</h2>
+            <h2 class="font-weight-light headline py-3">Privacy Policy</h2>
         </v-row>
         <v-row>
             <h3>Submissions and Comments</h3>
@@ -28,19 +28,125 @@ which is in our custody by emailing us at <a href="mailto:ripple.theteam@gmail.c
 We may request for certain personal information from you for the
 purposes of verifying your identity.</p>
             <h3>Data Retention</h3>
+            <p>RIPPLE will retain your personal information for as long as necessary to
+provide services users have requested (eg. newsletter, author profiles,
+etc). Personal information will also be retained and used as necessary 
+to comply with our legal obligations, resolve disputes, and enforce the
+agreements</p>
             <h3>Security</h3>
+            <p>We have implemented administrative, technical and physical
+safeguards to help prevent unauthorized access, use, or disclosure of
+personal information in our custody or control. Nonetheless, we cannot
+guarantee that these safeguards will prevent the unauthorized access,
+use or disclosure of the user’s personal information.</p>
             <h3>Persons Under 18</h3>
+            <p>Our site is not intended for persons under 18 and we do not knowingly
+collect personal information from persons under 18. If you are the
+parent or guardian of a person under 18 and believe that he or she has
+provided personal information to us, please contact us at
+<a href="mailto:ripple.theteam@gmail.com" target="_blank">ripple.theteam@gmail.com</a> and we will delete their information
+promptly.
+</p>
             <h3>Revisions to the Privacy Policy</h3>
-            <h2>Terms of User</h2>
+            <p>The Privacy Policy may be revised from time-to-time in RIPPLE’s sole
+discretion. A notice will be provided to you by posting a notice on the
+site before any material changes are made to our privacy practices.
+Please do visit this page and review our privacy policy periodically for
+changes. By continuing to access or use Ripple after changes become
+effective, users agree to be bound by terms of the revised privacy
+policy.
+</p>        
+            </v-row>
+            <v-row>
+            <h2 class="font-weight-light headline py-2">Terms of Use</h2>
+            </v-row>
+            <v-row>
             <h3>Conditions of Use of the Site</h3>
+            <p>RIPPLE is intended for persons who are 18 or older. If you are under 18,
+do not access or use the Site. The Site is offered subject to your 
+acceptance without modification of all the terms and conditions
+contained in this Agreement. By assessing or using any part of RIPPLE,
+you agree to be bound by the terms and conditions of the Agreement
+as such terms and conditions may be modified by us from time to time
+in our sole discretion. Please do not access the page if you don’t agree
+to the terms and conditions of RIPPLE. Do check the agreement
+periodically for changes. Continued access or use of Ripple following
+any changes to the Agreement constitutes your acceptance of those
+changes. </p>
             <h3>Copyright and Intelectual Property</h3>
+            <p>The Site and all of the articles, reviews, images, logos and other
+materials included on the site are protected by copyright and
+intellectual property laws. You may not reproduce, republish,
+distribute, display, sell or otherwise use any Site Content without
+written permission from RIPPLE. If you wish to request permission to
+use any Site Content, please email your request to us at
+<a href="mailto:ripple.theteam@gmail.com" target="_blank">ripple.theteam@gmail.com</a>.</p>
             <h3>Copyright Infringement</h3>
+            <p>Just as others respect out intellectual property rights, we respect the
+rights of others. Please notify us at <a href="mailto:ripple.theteam@gmail.com" target="_blank">ripple.theteam@gmail.com</a> should
+you believe that any Site content infringes upon your copy right. </p>
             <h3>User Content</h3>
+            <p>RIPPLE may post articles, reviews, comments and other materials from
+users and contributors to the Site. We are not the publisher or author
+of User Content and we may not be able to screen through all User
+Content before it is posted. However, we reserve the right in our sole
+discretion to remove User Content at any time and for any reason.
+You agree that any User Content you submit to the Site will not be
+unlawful, defamatory, or infringing on the privacy or intellectual
+property rights of others. You also agree (a) to be solely responsible for
+any damages resulting from any User Content you submit, (b) that all
+User Content you submit will be treated as non-proprietary and nonconfidential, and (c) not to submit any User Content that contains
+advertising or commercial solicitations. By submitting User Content to
+us, you automatically grant us the royalty-free, perpetual, worldwide,
+irrevocable, non-exclusive right to use, publish, reproduce, distribute,
+translate, and display User Content (including any drawings, images,
+sounds, video recordings, or other data embedded in the User Content
+and including adaptations and derivative works based on the User
+Content) for any purpose and in any manner or medium (now existing
+or hereafter developed).</p>
             <h3>Disclaimer of Warranties</h3>
+            <p>The Site may contain material that is offensive, objectionable as well as
+material containing technical inaccuracies, typographical mistakes, and
+other errors. We disclaim any responsibility for any harm resulting from
+access or use of the Site. The Site and all Site Content are provided “AS
+IS” to the full extent permitted by law. This means that we do not
+warrant that the Site or the Site Content are (1) fit for any particular
+purpose; (2) uninterrupted or error-free; or (3) free of defamatory,
+offensive, or illegal material or defects such as viruses, malfunctions, or
+harmful components that could damage or allow unauthorized access
+to your computer, computer network, or mobile device. </p>
             <h3>Limitation of Liability</h3>
+            <p>Neither do any parties involved in RIPPLE shall be liable for any direct,
+indirect, incidental or consequential damages resulting from the use or
+the inability to use the site or site content including, but not limited to
+(1) Damages caused by defamatory, offensive, or illegal material, (2) 
+damages caused by viruses, malfunctions, or harmful components
+accessed through the site, and (3) Damages for lost profits, loss of data,
+or other intangibles even if foreseeable or if we have been advised of
+the possibility of such damages.</p>
             <h3>Indemnification</h3>
+            <p>You agree to indemnify, hold harmless, and release RIPPLE and all
+parties involved from and against any and all claims, damages, costs,
+and expenses including, but not limited to, reasonable attorney’s fees
+arising from or related to your access, use, or misuse of the Site.
+</p>
             <h3>Links</h3>
+            <p>The Site may include links to other websites that are not owned or
+operated by us. RIPPLE do not have any control over these third party
+websites and are not responsible for any information, functionality, or
+content accessed through such websites. By linking to third party
+websites, we do not represent or imply that we endorse such websites.
+You are responsible for taking the necessary precautions to protect
+yourself and your computer from viruses, worms, and other harmful or
+destructive content that may be accessible through such web sites. We
+disclaim any responsibility for any harm resulting from your use of third
+party websites.</p>
             <h3>Changes</h3>
+            <p>We reserve the right, in our sole discretion, to modify or replace any
+part of the Agreement. It is your responsibility to check the Agreement
+periodically for changes. We may also, at any time and for any reason in
+our sole discretion, modify or discontinue the Site or Site Content or
+terminate or restrict your access to the Site.</p>
         </v-row>
     </v-container>
 </template>
