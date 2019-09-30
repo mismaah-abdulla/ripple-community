e92fa4448264245c49939c04239f213aec2efe15
mismaah-abdulla <mismaahabdulla@gmail.com>
2019-09-30 13:13:06 +0800

    add legal page

---
+++ src/views/Legal.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-container px-12 style="width: 60%">
+        <v-row justify="center">
+            <h1 class="display-1 font-weight-light">Legal Disclaimers<br></h1>
+        </v-row>
+        <v-row>
+            <h2 class="font-weight-light headline">Privacy Policy</h2>
+        </v-row>
+        <v-row>
+            <h3>Submissions and Comments</h3>
+            <p>Users can submit articles for publication at our Site by completing a
+submission form. The information you provide on the submission form
+is subjected to our privacy policy. Users may also post comments and
+other information at our Site through our comments platform.
+Comments posted on our Site are subject to our privacy policy as well.
+Our Site receives a significant amount of traffic and is accessible to
+anyone on the internet, therefore do not submit articles or post
+comments that are private and that you do not wish to be used or
+viewed by the public. Users are solely responsible and liable for any
+legal or other repercussions that arise as a result of the articles
+published or comments posted at our Site. We categorically disclaim
+any responsibility or liability for any damages suffered by the public
+and/or users arising from reliance on whatsoever articles or comments
+printed herein.</p>
+            <h3>Access to Personal Information</h3>
+            <p>Users may access and update inaccuracies in your personal information
+which is in our custody by emailing us at <a href="mailto:ripple.theteam@gmail.com" target="_blank">ripple.theteam@gmail.com</a>.
+We may request for certain personal information from you for the
+purposes of verifying your identity.</p>
+            <h3>Data Retention</h3>
+            <h3>Security</h3>
+            <h3>Persons Under 18</h3>
+            <h3>Revisions to the Privacy Policy</h3>
+            <h2>Terms of User</h2>
+            <h3>Conditions of Use of the Site</h3>
+            <h3>Copyright and Intelectual Property</h3>
+            <h3>Copyright Infringement</h3>
+            <h3>User Content</h3>
+            <h3>Disclaimer of Warranties</h3>
+            <h3>Limitation of Liability</h3>
+            <h3>Indemnification</h3>
+            <h3>Links</h3>
+            <h3>Changes</h3>
+        </v-row>
+    </v-container>
+</template>
+
+<style>
+p {
+  text-align: justify;
+}
+</style>
